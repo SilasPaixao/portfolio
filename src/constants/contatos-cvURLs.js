c51f39fd1a781f327491e8524ae1bf307c3a9a4b
SilasPaixao <silas.paixao873@gmail.com>
2023-02-02 18:06:14 -0300

    finalized

--- src/constants/contatos-cvURLs.js
+++ src/constants/contatos-cvURLs.js
@@ -1,3 +1,3 @@
 export const linkedinUlr = 'https://www.linkedin.com/in/silas-paix%C3%A3o-8a2a85205/';
 export const githubUrl = 'https://github.com/SilasPaixao';
-export const cvUrl = '#';
+export const cvUrl = 'https://drive.google.com/file/d/1se9MrFGA_Cv7KkmGCKZQVv5Q69c889yy/view?usp=sharing';
